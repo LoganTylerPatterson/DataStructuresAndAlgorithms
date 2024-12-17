3fa94a4f1aa740d9f7b664ec2aa15d31b6fa9b8e
Logan Patterson <67162730+LoganTylerPatterson@users.noreply.github.com>
2024-12-16 21:08:46 -0700

    heapsort started

--- C#/Sorting.cs
+++ C#/Sorting.cs
@@ -1,11 +1,10 @@
 using System;
 using System.Collections.Generic;
 
-public class Sorting
+public class QuickSort
 {
-
     // THE OG, THE QUICKSORT
-    public void QuickSort(List<int> array, int s, int e) 
+    public void QuickSort<T>(List<T> array, int s, int e) 
 	{
 		if (s < e) 
 		{
@@ -15,7 +14,7 @@ public class Sorting
 		}
 	}
 
-	public int Partition(List<int> array, int start, int end) 
+	public int Partition<T>(List<T> array, int start, int end) 
 	{
 		int ele = array[end];
 		int i = start - 1;
@@ -28,8 +27,25 @@ public class Sorting
 		array.Swap(i + 1, end);
 		return i + 1;
 	}
-	
-	
+}
+
+public class HeapSort<T> {
+	List<T> list;
+	public HeapSort(List<T> _list) {
+		list = _list;
+	}
+
+	private int GetParent(int i) {
+		return (i - 1 ) / 2;
+	}
+
+	private int GetLeftChild(int i) {
+		return i * 2 + 1;
+	}
+
+	private int GetRightChild(int i) {
+		return i * 2 + 2;
+	}
 }
 					
 public class Program
@@ -37,8 +53,7 @@ public class Program
 	public static void Main()
 	{
 		var test1 = new List<int>{4,2,1,3,5,6,2,8,1,9};
-		var Sorters = new Sorting();
-		Sorters.QuickSort(test1, 0, test1.Count-1);
+		test1.QuickSort();
 		for (int i = 0; i < test1.Count; i++) {
 			Console.WriteLine(test1[i]);
 		}
@@ -52,5 +67,14 @@ public static class ListExtensions
     T temp = array[i1];
     array[i1] = array[i2];
     array[i2] = temp;
+   }
+
+   public void QuickSort<T>(this List<T> array) {
+	var qs = new QuickSort();
+	qs.QuickSort(array);
+   }
+
+   public void HeapSort<T>(this List<T> array) {
+	var hs = new HeapSort(array);
    }
 }
