b6bb2df5d2a43a0a14517229e6775ac63ca36f96
Logan Patterson <lpatterson762@gmail.com>
2024-12-25 21:27:47 -0700

    added Counting Sort

--- C#/Sorting.cs
+++ C#/Sorting.cs
@@ -29,10 +29,41 @@ public class QuickSort
 	}
 }
 
-public class HeapSort<T> {
+public class CountingSort<T> {
+	public CountingSort<T>(List<T> _list, int k) {
+		List<int> C = new List<int>(k);
+		for(int i = 0; i < C.Count; i++) {
+			C[i] = 0;
+		}
+		
+		// count all the occurences of a character
+		for(int j = 0; j < _list.Count; j++) {
+			C[_list[i]] += 1;
+		}
+		
+		// at this point C[i] represents the count of the integer i
+		for(int i = 0; i < k; i++) {
+			C[i] = C[i] + C[i - 1];
+		}
+		
+		List<T> B = new List<T>(_list.Count);
+		// a this point C[i] contains the count of integer i + how many previous integers that come before
+		for(int i = 0; i < _list.Count; i++) {
+			B[C[_list[i]]] =  _list[i];
+			C[_list[i]] -= 1;
+		}
+
+		return B;
+	}
+}
+
+
+public class Heap<T> {
 	List<T> list;
-	public HeapSort(List<T> _list) {
+	int size;
+	public Heap<T>(List<T> _list) {
 		list = _list;
+		size = list.Count;
 	}
 
 	private int GetParent(int i) {
@@ -45,6 +76,38 @@ public class HeapSort<T> {
 
 	private int GetRightChild(int i) {
 		return i * 2 + 2;
+	}
+
+	private void SiftUp(int i) {
+		int parent = GetParent(i);
+		while (list[parent] < list[i]) {
+			list.Swap(parent, i);
+			i = parent;
+			parent = GetParent(i);
+		}
+	}
+
+	private void SiftDown(int i) {
+		int left = GetLeftChild(i);
+		int right = GetRightChild(i);
+		int largest = i;
+		if (list[left] > list[i]) {
+			largest = i;
+		}
+		if (list[right] > list[largest]) {
+			largest = right;
+		}
+		
+		if (i != largest) {
+			list.Swap(i, largest);
+			SiftDown(largest);
+		}
+	}
+
+	private void BuildMaxHeap() {
+		for(int i = size / 2; i > 0; i--) {
+			SiftDown(i);
+		}
 	}
 }
 					
@@ -77,4 +140,32 @@ public static class ListExtensions
    public void HeapSort<T>(this List<T> array) {
 	var hs = new HeapSort(array);
    }
+
+   public void CountingSort<T>(this List<T> _list, int k) {
+		List<int> C = new List<int>(k);
+
+		for(int i = 0; i < C.Count; i++) {
+			C[i] = 0;
+		}
+		
+		// count all the occurences of a character
+		for(int j = 0; j < _list.Count; j++) {
+			C[_list[i]] += 1;
+		}
+		
+		// at this point C[i] represents the count of the integer i
+		for(int i = 0; i < k; i++) {
+			C[i] = C[i] + C[i - 1];
+		}
+		
+		List<T> B = new List<T>(_list.Count);
+		// a this point C[i] contains the count of integer i + how many previous integers that come before
+		for(int i = 0; i < _list.Count; i++) {
+			B[C[_list[i]]] =  _list[i];
+			C[_list[i]] -= 1;
+		}
+
+		_list = B;
+	}
+
 }
